db06b058ea010f9b2888fae458c6a6ed70bbb64f
yaneproger@gmail.com <yaneproger@gmail.com>
2023-12-18 18:15:10 -0500

    191223-03:17-commit

--- Old_Pyhton_work/main.py
+++ Old_Pyhton_work/main.py
@@ -1016,18 +1016,8 @@ print(year % 4 == 0 and year % 100 != 0 or year % 400 == 0)
 # print(        n%4==0 and n%400 ==0  or n%100 !=0        )
 
 
-
-
-
-
-
-
-
-
-
-
-
-
+# n = int (input("Enter a number"))
+# print(        n%4==0 and n%400 ==0  or n%100 !=0        )
 
 
 
