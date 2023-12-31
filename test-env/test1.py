3f89eb0ec1f22486ffcb59c342f7ef36de5f40a1
yaneproger@gmail.com <yaneproger@gmail.com>
2023-12-30 20:00:37 -0500

    123123-05:01-commit

---
+++ test-env/test1.py
@@ -0,0 +1,291 @@
+
+
+"""
+"""
+
+# import abc
+
+
+# name = input("Enter your name: ")
+# age = input("Enter your age: ")
+# town = input("Enter your town: ")
+# print(name,age,town)
+# print(name.find('a'))
+"""
+def new_func(name, town):
+    print(name.upper(),town)
+
+name = input("Enter your name: ")
+town = input("Enter your town: ")
+new_func(name, town)
+
+
+
+#print(dir(name))
+#print(dir(int))
+
+
+
+import math
+
+
+def my_name(name1):
+      print(name1)
+
+
+my_name('Eldar')
+
+
+#my_name('Eldar')
+
+def foo(x):
+    if x >= 0:
+        return math.sqrt(x)
+    else:
+        return None
+
+def bar(x):
+    if x < 0:
+        return None
+    return math.sqrt(x)
+var1 = input ("enter number:" )
+
+print(bar(var1))
+
+
+
+my_list = [1, 2, 3, 4]
+
+print(my_list)
+
+
+a = 10
+
+
+def new_fn():
+    print('Hello', 'Eldar')  # comments
+
+    # this
+    # is
+    # comments
+
+# this   - Ctrl + / makes auto comment
+# is   - use Ctrl + / - to disable comment
+#  comment
+
+
+new_fn()       # This is inline comment
+
+print(10)
+
+
+print(print(10+5))
+
+print(input("Enter ur name: "))
+
+print(def my_fn():
+      print('Eldar'))
+
+print(if True print(10))
+
+import datetime
+print(print(10+5))
+
+
+print(datetime.MAXYEAR)
+print(datetime.MINYEAR)
+"""
+# print(import datetime)
+
+
+# import datetime
+
+
+# def time(self: datetime.
+#          datetime(year=2023, month=03, day=22,
+#                   hour=07, minute=27, second=10)):
+
+#     print(datetime.date(),
+#           datetime.time())
+
+
+# time
+
+"""
+
+
+def newfn(a, b, c):
+    var = print(a/b/c)
+    return var
+
+
+def my_fn(x, y):
+    name = print(x+x*y)
+    return name
+
+
+# print(my_fn(x=10, y=10))
+# print(newfn(a=1, b=2, c=5))
+newfn(1, 2, 5)  # newfn(a=1, b=2, c=5)
+my_fn(1, 2)
+
+{abba: 1, betta: 2}
+
+my_list = [1, 2, 3, 4, 5]
+
+
+def new_fn():
+    print('Hello!')
+    x = 1
+    y = 2
+    z = 3
+    return x + y + z
+
+
+print(new_fn)
+
+
+print(my_list)
+print(my_dict)
+
+my_first_logical_type = True
+
+print(my_first_logical_type)
+
+x = 11
+
+
+def test_log_type():
+    if x > 10:
+        return True
+
+
+print(test_log_type())
+
+
+my_first_variable_number = 1
+
+print(my_first_variable_number)
+my_first_variable_string = 'String1'
+
+print(my_first_variable_string)
+
+
+my_dictionary = {'abba': 1, 'betta': 2, 'name': 'Eldar'}
+print(my_dictionary)
+my_number = 10
+print(my_number)
+
+
+
+def print_name(name):
+    print(name)
+
+
+print_name('Eldar')
+
+# print_name = 10
+
+
+print(print_name('Eldar'))
+
+
+a = 1
+b = 100
+c = 20
+d = 40
+e = 150
+max = a
+if b > max:
+    max = b
+if c > max:
+    max = c
+if d > max:
+    max = d
+if e > max:
+    max = e
+print(max)
+
+my_name = 'Eldar'
+print(id(my_name))
+
+
+my_num = 100
+
+print(id(my_num))
+
+other_num = my_num
+print(id(other_num))
+
+"""
+"""
+# max number from five numbers
+a = 1
+b = 8
+c = 3
+d = 2
+e = 6
+max = a
+
+if b > max:
+    max = b
+if c > max:
+    max = c
+if d > max:
+    max = d
+if e > max:
+    max = e
+print(max)
+
+# even numbers summ 1to15
+I = 2
+N = 0
+while I <= 15:
+    N = N+I
+    I = I+2
+print(N)
+"""
+"""
+# calculate even numbers from entered to 15
+s = 0
+i = int(input("add entered even number to another 15 times :"))
+if i > 15:
+    print(i, 'enter number under 15')
+# i = 0
+# positive_float = float('inf')
+if i % 2 == 0:
+    while i <= 15:
+        s = s+i
+        i = i+2
+else:
+    print(i, 'is an odd number')
+print(s)
+
+"""
+# number = int(input("enter number :"))
+
+# if number % 2 == 0:
+
+#     print(number, (' четное число'))
+# else:
+#     print(number, (' нечетное число'))
+
+    # else:
+
+#     print(number, ('четное число'))
+
+
+# min value from four number
+
+# A = 1
+# B = 2
+# C = 3
+# D = 4
+# Min = A
+# if B < Min:
+#     Min = B
+# if C < Min:
+#     Min = C
+# if D < Min:
+#     Min = D
+# print(Min)
