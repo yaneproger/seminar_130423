e6c75ebeb5f959ccf7649d8e8fdff7083df9232f
yaneproger@gmail.com <yaneproger@gmail.com>
2024-01-04 00:41:53 -0500

    040124-09:43-commit

--- SeminarsSince101223/main.py
+++ SeminarsSince101223/main.py
@@ -1311,4 +1311,98 @@ print('MaxElementsValue', count_maximal, 'ArraySize', len(numbers),
 # Пример:
 
 # k = 'ноутбук'
-# # 12
+# # # 12
+
+# list_1 = [1, 4, 3, 7, 8, 9, 2]
+# k = 8
+
+# data = [str(i) for i in input("Введите числа: ").split()]
+# print(data)
+
+
+# for i in range(len(list_1)):
+#     if list_1[i] == k:
+#         res = list_1[i]
+#     else:
+#         if list_1[i] == k+1 or list_1[i] == k-1:
+#             res1 = list_1[i]
+# if k == res:
+#     res = res
+# else:
+#     res = res1
+
+# print(res)
+
+
+# n = int(input("Введите кол-во элементов списка: "))
+# data = []
+# for i in range(n):
+#     data.append(str(input("Введите число: ")))
+#     # insert(position, element)
+# print(data)
+
+
+# print(" !! ".join(data))
+
+# A, E, I, O, U, L, N, S, T, R – 1
+
+# data = [{"A, E, I, O, U, L, N, S, T, R": "1"}, {"V": "S002"}, {"VI": "S001"},
+#         {"VI": "S005"}, {"VII": "S005"}, {"V ": "S009"}, {"VIII": "S007 "}]
+
+# print(data[0]["A, E, I, O, U, L, N, S, T, R"])
+
+wordsCost = 0
+wordsdict = {}
+# print(type(wordsdict))
+
+k = [str(i.capitalize()) for i in input("Введите числа: ").split()]
+print(k)
+
+wordsdict = {"A": "1", "E": "1", "O": "1", "U": "1", "L": "1", "N": "1",
+             "S": "1", "T": "1", "R": "1", "D": "2", "G": "2", "B": "3",
+             "C": "3", "M": "3", "P": "3", "F": "4", "H": "4", "V": "4",
+             "W": "4", "Y": "4", "K": "5", "J": "8", "X": "8", "Q": "10", "Z": "10",
+
+             "А": "1", "В": "1", "Е": "1", "И": "1", "Н": "1", "О": "1",
+             "Р": "1", "С": "1", "Т": "1",
+             "Д": "2", "К": "2", "Л": "2", "М": "2", "П": "2", "У": "2",
+             "Б": "3", "Г": "3", "Ё": "3", "Ь": "3", "Я": "3",
+
+             "Й": "4", "Ы": "4",
+
+             "Ж": "5", "З": "5", "Х": "5", "Ц": "5", "Ч": "5",
+             "Ш": "8", "Э": "8", "Ю": "8",
+                            "Ф": "10", "Щ": "10", "Ъ": "10"
+
+             }
+# print(type(wordsdict))
+for el in k:
+    if el in wordsdict:
+
+        # print(wordsdict.get(el))
+
+        temp = int(wordsdict.get(el))
+        # print(temp)
+
+        wordsCost = wordsCost + temp
+
+
+print(wordsCost)
+
+
+# А, В, Е, И, Н, О, Р, С, Т – 1 очко;
+# Д, К, Л, М, П, У – 2 очка;
+# Б, Г, Ё, Ь, Я – 3 очка;
+# Й, Ы – 4 очка;
+# Ж, З, Х, Ц, Ч – 5 очков;
+# Ш, Э, Ю – 8 очков;
+# Ф, Щ, Ъ – 10 очков.
+
+
+# A, E, I, O, U, L, N, S, T, R – 1 очко;
+# D, G – 2 очка
+# B, C, M, P – 3 очка;
+# F, H, V, W, Y – 4 очка;
+# K – 5 очков;
+# J, X – 8 очков;
+# Q, Z – 10 очков.
